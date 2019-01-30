43e3789a5fd33e70d1ce543b81eb34b25704ebc1
semvs <semvs@ayvansaray.edu.tr>
2019-01-30 13:56:54 +0300

    linq to sql giris

---
+++ LinqToSQLGiris/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace LinqToSQLGiris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboTedarikci = new System.Windows.Forms.ComboBox();
+            this.comboKategori = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.nudStok = new System.Windows.Forms.NumericUpDown();
+            this.nudFiyat = new System.Windows.Forms.NumericUpDown();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudStok)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFiyat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 66);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(680, 292);
+            this.dataGridView1.TabIndex = 18;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // comboTedarikci
+            // 
+            this.comboTedarikci.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboTedarikci.FormattingEnabled = true;
+            this.comboTedarikci.Location = new System.Drawing.Point(411, 38);
+            this.comboTedarikci.Name = "comboTedarikci";
+            this.comboTedarikci.Size = new System.Drawing.Size(89, 21);
+            this.comboTedarikci.TabIndex = 16;
+            // 
+            // comboKategori
+            // 
+            this.comboKategori.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboKategori.FormattingEnabled = true;
+            this.comboKategori.Location = new System.Drawing.Point(310, 38);
+            this.comboKategori.Name = "comboKategori";
+            this.comboKategori.Size = new System.Drawing.Size(89, 21);
+            this.comboKategori.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(307, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Kategori:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(408, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Tedarikci:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(222, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Stok:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(117, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Fiyat:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Ürün Adı:";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(506, 38);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(58, 22);
+            this.btnEkle.TabIndex = 10;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // nudStok
+            // 
+            this.nudStok.Location = new System.Drawing.Point(217, 38);
+            this.nudStok.Maximum = new decimal(new int[] {
+            500000,
+            0,
+            0,
+            0});
+            this.nudStok.Name = "nudStok";
+            this.nudStok.Size = new System.Drawing.Size(87, 20);
+            this.nudStok.TabIndex = 9;
+            // 
+            // nudFiyat
+            // 
+            this.nudFiyat.Location = new System.Drawing.Point(118, 38);
+            this.nudFiyat.Maximum = new decimal(new int[] {
+            50000,
+            0,
+            0,
+            0});
+            this.nudFiyat.Name = "nudFiyat";
+            this.nudFiyat.Size = new System.Drawing.Size(93, 20);
+            this.nudFiyat.TabIndex = 8;
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(12, 37);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(100, 20);
+            this.txtProductName.TabIndex = 7;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(570, 37);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(58, 22);
+            this.btnSil.TabIndex = 10;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(634, 38);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(58, 21);
+            this.btnKaydet.TabIndex = 10;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 383);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Ürün Adına göre ara";
+            // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(120, 380);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(100, 20);
+            this.txtAra.TabIndex = 7;
+            this.txtAra.TextChanged += new System.EventHandler(this.txtAra_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 414);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboTedarikci);
+            this.Controls.Add(this.comboKategori);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.nudStok);
+            this.Controls.Add(this.nudFiyat);
+            this.Controls.Add(this.txtAra);
+            this.Controls.Add(this.txtProductName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudStok)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFiyat)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboTedarikci;
+        private System.Windows.Forms.ComboBox comboKategori;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.NumericUpDown nudStok;
+        private System.Windows.Forms.NumericUpDown nudFiyat;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtAra;
+    }
+}
+
---
+++ LinqToSQLGiris/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqToSQLGiris
+{
+    public partial class Form1 : Form
+    {
+        NorthWindDataContext ctx = new NorthWindDataContext();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            Product p = new Product();
+            p.ProductName = txtProductName.Text;
+            p.UnitPrice = nudFiyat.Value;
+            p.UnitsInStock = Convert.ToInt16(nudStok.Value);
+            p.CategoryID = (int)comboKategori.SelectedValue;
+            p.SupplierID = (int)comboTedarikci.SelectedValue;
+
+            ctx.Products.InsertOnSubmit(p);
+            MessageBox.Show($"SubmitChanges oncesi ProductID={p.ProductID}");
+            ctx.SubmitChanges();// değişiklikleri ADO.Net koduna çevirerek veritabanına gönder
+            MessageBox.Show($"SubmitChanges sonrası ProductID={p.ProductID}");
+
+            //refresh the grid
+            //GetNewBindingList-> eklediğimiz datagridviewde görünmesi için.
+            dataGridView1.DataSource = ctx.Products.GetNewBindingList();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //data gridvievde productsu listeledi otomatik.
+            dataGridView1.DataSource = ctx.Products;
+
+            comboKategori.DisplayMember = "CategoryName";
+            comboKategori.ValueMember = "CategoryID";
+            comboKategori.DataSource = ctx.Categories;
+
+            comboTedarikci.DisplayMember = "CompanyName";
+            comboTedarikci.ValueMember = "SupplierID";
+            comboTedarikci.DataSource = ctx.Suppliers;
+
+            //Kategori ve tedarikci adnın grigcieve gelmesi için
+            //kategori ve tedarikçi adinin adatgridview'a gelemesi için sunları ekledik:
+            //product ile category tablolarını join et;
+            var sonuc = from urun in ctx.Products
+                        join kat in ctx.Categories
+                        on urun.CategoryID equals kat.CategoryID
+                        select new // asağıdaki bes kolondan olusan yeni bir entity tipi.(anonim)
+                        {
+                            urun.ProductID,
+                            urun.ProductName,
+                            urun.UnitPrice,
+                            urun.UnitsInStock,
+                            kat.CategoryName
+                        };
+            //dataGridView1.DataSource = null;
+            //dataGridView1.DataSource = sonuc;
+            dataGridView1.DataSource = ctx.Products.GetNewBindingList();
+          
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null)
+                return;
+            //once contex ten sil sonra onayla veritabanından sil
+            int urunID = (int)dataGridView1.CurrentRow.Cells["ProductId"].Value;
+            Product p = ctx.Products.SingleOrDefault(urun => urun.ProductID == urunID);
+            ctx.Products.DeleteOnSubmit(p);
+            ctx.SubmitChanges();
+
+            //refresh the grid
+            dataGridView1.DataSource = ctx.Products.GetNewBindingList();
+          
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow r = dataGridView1.CurrentRow;
+            txtProductName.Text = r.Cells["ProductName"].Value.ToString();
+            nudFiyat.Value = Convert.ToDecimal(r.Cells["UnitPrice"].Value);
+            nudStok.Value = Convert.ToDecimal(r.Cells["UnitsInStock"].Value);
+            comboKategori.SelectedValue = (int)r.Cells["CategoryId"].Value;
+            comboTedarikci.SelectedValue = (int)r.Cells["SupplierId"].Value;
+            txtProductName.Tag = r.Cells["ProductId"].Value;
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            Product p = ctx.Products.SingleOrDefault(urun => urun.ProductID == (int)txtProductName.Tag);
+            p.ProductName = txtProductName.Text;
+            p.UnitPrice = nudFiyat.Value;
+            p.UnitsInStock = Convert.ToInt16(nudStok.Value);
+            p.CategoryID = (int)comboKategori.SelectedValue;
+            p.SupplierID = (int)comboTedarikci.SelectedValue;
+            ctx.SubmitChanges();//değişiklikleri ADO.net koduna çevirerek veritabanına gönder
+                                //refresh the grid
+            dataGridView1.DataSource = ctx.Products.GetNewBindingList();
+        }
+
+        private void txtAra_TextChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = ctx.Products.Where(x => x.ProductName.Contains(txtAra.Text));
+        }
+    }
+}
